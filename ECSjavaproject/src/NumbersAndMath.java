1cb0846dd17ba1c45e08178c31bb7951debb0768
Colin Roke <rokec8039@my.sduhsd.net>
2017-12-01 11:11:59 -0800

    j

---
+++ ECSjavaproject/src/NumbersAndMath.java
@@ -0,0 +1,30 @@
+public class NumbersAndMath
+{
+	public static void main(String[] args)
+	{
+		System.out.println("I will now count my chickens:");
+		
+		System.out.println("Hens " + ( 25 + 30 / 6 ) );
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
